e92027865296d9eea3d466ad25ed4d3d33323c70
Cody Lobdell <thecreator85@gmail.com>
2023-01-24 19:15:56 -0800

    Finished javascript questions

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
 'use strict';
+
+console.log('Hello, who are you?');
+alert('Hi there! Welcome to my page!');
+let username = prompt('What is your name?');
+alert('Hello ' + username + ', thank you for stopping by!');
+
+let amiyoung = prompt('Am I younger than 35? Yes or No?');
+
+if (amiyoung === 'No') { 
+  console.log('You are right! I am older than 35')
+}
+
+let amitall = prompt('Am I short?');
+
+if (amitall === 'No') { 
+  console.log('Correct! I am pretty tall')
+}
+
+let didilive = prompt('Did I live in Korea?');
+
+if (didilive === 'Yes') { 
+  console.log('Yes, I did! Big shout out to the Army! Haha')
+}
+
+let doiownpets = prompt('Do I own any pets?');
+
+if (doiownpets === 'No') { 
+  console.log('Unfortunately, I dont own any pets')
+}
+
+let doilikegreen = prompt('Do I like the color green?');
+
+if (doilikegreen === 'Yes') { 
+ console.log('Yes, green is one of my favorite colors!')
+}
