af1eafc09810c5ca2c54d8f17df98e3337f4515d
Cody Lobdell <thecreator85@gmail.com>
2023-01-25 12:54:48 -0800

    added top ten list and cleaned up js formating

--- js/app.js
+++ js/app.js
@@ -8,52 +8,52 @@ alert('Hello ' + username + ', thank you for stopping by!');
 
 let amiyoung = prompt('Am I younger than 35? Yes or No?');
 
-if (amiyoung === 'No') { 
+if (amiyoung === 'No') {
   console.log('You are right! I am older than 35')
 }
 
-if (amiyoung === 'Yes') { 
+if (amiyoung === 'Yes') {
   console.log('Sorry! I am older than 35!')
 }
 
 
 let amitall = prompt('Am I short?');
 
-if (amitall === 'No') { 
+if (amitall === 'No') {
   console.log('Correct! I am pretty tall')
 }
 
-if (amitall === 'Yes') { 
+if (amitall === 'Yes') {
   console.log('I am definitely over six foot!')
 }
 
 
 let didilive = prompt('Did I live in Korea?');
 
-if (didilive === 'Yes') { 
+if (didilive === 'Yes') {
   console.log('Yes, I did! Big shout out to the Army! Haha')
 }
-if (didilive === 'No') { 
+if (didilive === 'No') {
   console.log('Sorry, the answer is yes! I lived there for three years!')
 }
 
 
 let doiownpets = prompt('Do I own any pets?');
 
-if (doiownpets === 'No') { 
+if (doiownpets === 'No') {
   console.log('Unfortunately, I dont own any pets')
 }
 
-if (doiownpets === 'Yes') { 
+if (doiownpets === 'Yes') {
   console.log('That is incorrect. I dont have any pets.')
 }
 
 let doilikegreen = prompt('Do I like the color green?');
 
-if (doilikegreen === 'Yes') { 
- console.log('Yes, green is one of my favorite colors!')
+if (doilikegreen === 'Yes') {
+  console.log('Yes, green is one of my favorite colors!')
 }
 
-if (doilikegreen === 'No') { 
+if (doilikegreen === 'No') {
   console.log('Sorry! Green is actually one of my favorite colors!')
- }
+}
