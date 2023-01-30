d26321ea4c4dab34e70e6be5615f1f30a752a826
Cody A. Lobdell <115486730+CodyLobdell@users.noreply.github.com>
2023-01-29 20:40:56 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,109 +1,143 @@
 'use strict';
 
-let username = prompt("Hello, who are you?");
-console.log("Hello, who are you?");
-alert("Hello " + username + " thank you, for stopping by");
+alert('Welcome to my page!');
+console.log('Welcome to my page!');
 
-alert("Lets answer some quick questions! Type y for yes, or n for no!");
+let username = prompt('What is your name?');
+console.log('What is your name?');
+alert('Hello ' + username + ', thank you for stopping by!');
+alert('Lets answer some quick questions! Type y for yes, or n for no!');
 
-function whatMyAge() {
-  let age = prompt("Do you think I'm younger than 35?");
+let age = prompt('Do you think I am younger than 35?').toLowerCase();
 
-  if (age.toUpperCase() === 'Yes' || age.toUpperCase() === 'Y') {
-    console.log("What's my age again?");
-    alert("Sorry, I am older than 35!")
-  } else if (age.toUpperCase() === 'No' || age.toUpperCase() === 'N') {
-    console.log('That is right! I am almost 40! But I still feel young!')
-    alert('That is right! I am almost 40! But I still feel young!')
+function howOld() {
+  if (age === 'yes' || age === 'y') {
+    console.log('Incorrect! I am actually 37!');
+    alert('Incorrect! I am actually 37!');
+  } else if (age === 'no' || age === 'n') {
+    console.log('Great job, you are correct!');
+    alert('Great job, you are correct!');
   } else {
-    console.log('Is 40 still the new 30?')
-    alert("Is 40 still the new 30?")
-  };
+    console.log('Sorry, that is not correct.');
+    alert('Sorry, that is not correct.');
+  }
 }
+
+howOld(age);
+
+let tall = prompt('Am I very short?').toLowerCase();
 
 function whatsMyHeight() {
-  let height = prompt("Am I short?");
-
-  if (height.toUpperCase() === 'Yes' || height.toUpperCase() === 'Y') {
-    console.log('What is my height?');
-    alert("False! At least I dont think I am short")
-  } else if (height.toUpperCase() === 'No' || height.toUpperCase() === 'N') {
-    console.log("What's my height?")
-    alert("Correct! I am actually over six foot!")
-  };
+  if (tall === 'yes' || tall === 'y') {
+    console.log('False! At least I dont think I am short...');
+    alert('False! At least I dont think I am short...');
+  } else if (tall === 'no' || tall === 'n') {
+    console.log('Correct! I am actually over six foot!');
+    alert('Correct! I am actually over six foot!');
+  } else {
+    console.log('Sorry, that is not correct.');
+    alert('Sorry, that is not correct.');
+  }
 }
 
-function whereDoILive(){
-  let live = prompt("Did I ever live in Korea?");
+whatsMyHeight(tall);
 
-  if (live.toUpperCase() === 'Yes' || live.toUpperCase() === 'Y') {
-    console.log('Did I live in Korea?');
-    alert("Yes, I did! Big shout out to the Army! Haha")
-  } else if (live.toUpperCase() === 'No' || live.toUpperCase() === 'N') {
-    console.log("Sorry, but I did live in Korea!")
-    alert("Sorry, wrong answer! I live in Korea for three years!")
-  };
+let live = prompt('Did I ever live in Korea?').toLowerCase();
+
+function whereDoILive() {
+  if (live === 'yes' || live === 'y') {
+    console.log('Yes, I did! Big shout out to the Army! Haha');
+    alert('Yes, I did! Big shout out to the Army! Haha');
+  } else if (live === 'no' || live === 'n') {
+    console.log('Sorry, that is wrong. I DID live in Korea!');
+    alert('Sorry, that is wrong. I DID live in Korea!');
+  } else {
+    console.log('Sorry, that is not correct.');
+    alert('Sorry, that is not correct.');
+  }
 }
 
-function doIHavePets () {
-  let pet = prompt("Do I own any pets?");
+whereDoILive(live);
 
-  if (pet.toUpperCase() === 'Yes' || pet.toUpperCase() === 'Y') {
-    console.log('Do I own any pets?')
-    alert("That is wrong. Unfortunately, I dont own any pets.")
-  } else if (pet.toUpperCase() === 'No' || pet.toUpperCase() === 'N') {
-    console.log("Do I own any pets?.")
-    alert("Correct! I do not have any pets currently. Sad day.")
-  };
+let pets = prompt('Do I own any pets?')
+  .toLowerCase();
+
+function doIHavePets() {
+  if (pets === 'yes' || pets === 'y') {
+    console.log('That is wrong. Unfortunately, I dont own any pets.');
+    alert('That is wrong. Unfortunately, I dont own any pets.');
+  } else if (pets === 'no' || pets === 'n') {
+    console.log('Correct! I do not have any pets currently. Sad day.');
+    alert('Correct! I do not have any pets currently. Sad day.');
+  } else {
+    console.log('Sorry, that is not correct.');
+    alert('Sorry, that is not correct.');
+  }
 }
+
+doIHavePets(pets);
+
+let color = prompt('Do I like the color green?')
+  .toLowerCase();
 
 function doIlikeGreen() {
-  let color = prompt("Do I like the color green?");
-
-  if (color.toUpperCase() === 'Yes' || color.toUpperCase() === 'Y') {
-    console.log('Do I like the color green??')
-    alert("Correct! Green is one of my favorite colors!")
-  } else if (pet.toUpperCase() === 'No' || pet.toUpperCase() === 'N') {
-    console.log("Do I own any pets?.")
-    alert("Sorry, that is wrong. I LOVE green!")
+  if (color === 'yes' || color === 'y') {
+    console.log('Yes, I LOVE the color green!');
+    alert('Yes, I LOVE the color green!');
+  } else if (color === 'no' || color === 'n') {
+    console.log('That is wrong. Green is one of my favorite colors!');
+    alert('That is wrong. Green is one of my favorite colors!');
   } else {
-    console.log('Maybe I will wear green next week!')
-    alert('Maybe I will wear green next week!')
-  };
-}
-
-function guessANumber() {
-  let randomNumber = getRandomNumber(10);
-  let guess;
-  let guessCount = 0;
-  let correctGuess = false;
-
-  function getRandomNumber(upper) {
-    let num = Math.floor(Math.random() * upper) + 1;
-    return num;
+    console.log('Sorry, that is not correct.');
+    alert('Sorry, that is not correct.');
   }
-
-  do {
-    guess = prompt('I am thinking of a number between 1 and 10. What is it?');
-    guessCount += 1;
-    if (parseInt(guess) === randomNumber) {
-      correctGuess = true;
-    }
-
-  } while (!correctGuess)
-  alert('You guessed the number!');
-  alert('It took you ' + guessCount + ' tries to guess the number ' + randomNumber + '.');
 }
 
-whatMyAge();
-whatsMyHeight();
-whereDoILive();
-doIHavePets();
-doIlikeGreen();
-guessANumber();
+doIlikeGreen(color);
 
+function numberGuess() {
+  let numberinput = parseInt(prompt('Guess my favorite number! It is a number between 1 - 20! '));
+  let x = 19;
+  let i = 4;
+  while (i > 0) {
+    if (numberinput === x) {
+      alert('Bingo, Good guess! You are good at this!');
+      break;
+    }
+    else if (numberinput < x) {
+      i--;
+      numberinput = parseInt(prompt(`Too low! Try guessing higher. You only have ${i} guesses left!`));
+    }
+    else if (numberinput > x) {
+      i--;
+      numberinput = parseInt(prompt(`Too high! try guessing lower. You only have ${i} guesses left!`));
+    }
+    else {
+      i--;
+      numberinput = parseInt(prompt(`Only a number between 1-20, please! You have ${i} guesses left!`));
+    }
+    if (i === 0) {
+      alert('You are all out of guesses! The correct answer was 19!');
+    }
+  }
+}
+numberGuess();
 
+function foodGuess() {
+  let possibleAnswers = ['pizza', 'sushi', 'cheeseburgers', 'salads', 'cookies', 'kale'];
+  let userAttempts = 6;
 
+  alert(`Guess my favorite food! You only have ${userAttempts} guesses left!`);
 
-
-alert("You have 6 chances to guess my favorite food!");
+  for (let i = 0; i < userAttempts; i++) {
+    let userGuess = prompt(`Guess my favorite food! You have ${userAttempts - i} guesses left!`);
+    if (possibleAnswers.includes(userGuess)) {
+      alert(`Great job! You guessed one of my favorite foods! Some of my favorite foods are: ${possibleAnswers.join(', ')}`);
+      break;
+    }
+    if (i === userAttempts - 1) {
+      alert(`Sorry, you are all out of guesses!! Some of my favorite foods are: ${possibleAnswers.join(', ')}`);
+    }
+  }
+}
+foodGuess();
